dddb59a012fb88e70c4cfde22f6af12b6c352478
Ramandeep Malhotra <r.malhotra@adform.com>
2021-07-23 13:53:45 +0530

    updated secret for production domain

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,8 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
 
-// const domain = process.env.PRODUCTION_DOMAIN;
-const domain = 'https://d2i0cep6siore0.cloudfront.net';
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
